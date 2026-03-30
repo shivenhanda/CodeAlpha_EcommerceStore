46827f3a49920ea957ca69fc361228f745f8a75d
shivenhanda <shivenhanda2003@gmail.com>
2026-03-30 22:05:33 +0530

    fix:Wishlist Page

--- frontend/src/context/WishListPage.jsx
+++ frontend/src/context/WishListPage.jsx
@@ -4,7 +4,7 @@ import { wishListContext } from "./WishList"
 
 export default function WishListPage() {
     const { width,product } = useContext(wishListContext)
-    if (product.length === 0) return <p>No products found</p>;
+    if (product.length === 0) return <p className="text-center">No products found</p>;
     console.log(product.length)
     return (
         <>
