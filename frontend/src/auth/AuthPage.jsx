d91f96eda49d4f060002f19a5f5daa4dc82928a6
shivenhanda <shivenhanda2003@gmail.com>
2026-04-05 19:27:25 +0530

    feat: connect signup login with backend DB

--- frontend/src/auth/AuthPage.jsx
+++ frontend/src/auth/AuthPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Signup from './Signup'
+import Login from './Login'
 
 export default function AuthPage() {
   return (
     <div>
         <div>
             <Signup/>
+            <Login/>
         </div>
     </div>
   )
--- frontend/src/auth/Signup.jsx
+++ frontend/src/auth/Signup.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import {useForm} from 'react-hook-form'
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState:{errors}
+  }=useForm()
+  const onSubmit=async (data)=>{
+    try{
+      let res=await fetch("http://localhost:8000/api/SignUp",{
+        method:"post",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        credentials: "include",
+        body:JSON.stringify(data)
+      })
+      res=await res.json();
+      if(!res.success){
+        alert(res.message)
+      }
+      else{
+        alert(res.message)
+      }
+    }
+    catch(error){
+      console.log("error",error)
+    }
+  }
   return (
-    <form method="post" action="/signup" className="flex justify-center items-center flex-col gap-1">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center items-center flex-col gap-1 bg-white">
+      <h1 className='border border-black rounded-xl text-center text-3xl p-1 m-3'>Signup</h1>
       <label htmlFor="user">Enter Username</label>
-      <input type="text" name="user" id="user" className="border border-black rounded-xl text-center"/>
+      <input id="user" {...register("user",{required:true})} className="border border-black rounded-xl text-center"/>
+      {errors.user && <span>This is required field</span>}
       <label htmlFor="email">Enter Email</label>
-      <input type="text" name="email" id="email" className="border border-black rounded-xl text-center"/>
+      <input id="email" {...register("email",{required:true})} className="border border-black rounded-xl text-center"/>
+      {errors.email && <span>This is required field</span>}
       <label htmlFor="password">Enter Password</label>
-      <input type="text" name="password" id="password" className="border border-black rounded-xl text-center"/>
+      <input id="password" {...register("password",{required:true})} className="border border-black rounded-xl text-center"/>
+       {errors.password && <span>This is required field</span>}
       <button className="border border-black rounded-xl text-center p-2">Signup</button>
     </form>
   )
